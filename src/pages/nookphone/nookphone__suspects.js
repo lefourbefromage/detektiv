d281bad57d14331a2aebcb850276e1da2b2ca1ac
lefourbefromage <vincentw@jacquieetmichel.fr>
2021-09-28 14:09:36 +0200

    add suspect design

--- src/pages/nookphone/nookphone__suspects.js
+++ src/pages/nookphone/nookphone__suspects.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import NookPhone from './nookphone';
+import userData from '../../data/suspects.json';
 
-const NookPhoneSuspects = () => {
+const NookPhoneInstanook = () => {
     return (
-        <NookPhone view="messages">
-          <div class="app suspects">
-            <h1 class="app__title">Liste des suspects</h1>
-            </div>
+        <NookPhone view="suspects app">
+          <h1 class="app__title">Suspects</h1>
+          <div class="suspects__list">
+            {userData.map((userDetail, index)=>{
+            return (
+              <div class="suspects__item">
+                  <div class="suspects__avatar">
+                      <img src={userDetail.avatar} alt={userDetail.username}/>
+                  </div>
+                  <div class="suspects__informations">
+                      <h2 class="suspects__username">{userDetail.username}</h2>
+                      <p class="suspects__job">{userDetail.job}</p>
+                      <p class="suspects__context">{userDetail.text}</p>   
+                  </div>
+              </div>
+            )
+          })}
+          </div>
         </NookPhone>
     );
 };
 
-export default NookPhoneSuspects;
+export default NookPhoneInstanook;
