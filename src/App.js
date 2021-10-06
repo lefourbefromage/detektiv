230da9a43af48aa75786910fd5a14b96ab2e4d57
lefourbefromage <vincentw@jacquieetmichel.fr>
2021-10-06 12:02:25 +0200

    add video and fix avatar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NookPhoneSuspects from "./pages/nookphone/nookphone__suspects";
 import NookPhoneInstaNook from "./pages/nookphone/nookphone__instanook";
 import NookPhoneWeather from "./pages/nookphone/nookphone__weather";
 import NookPhoneMaps from "./pages/nookphone/nookphone__maps";
+import NookPhoneStreetVideo from "./pages/nookphone/nookphone__street";
 
 
 
@@ -21,6 +22,8 @@ function App() {
             <Route path="/detective" exact component={JQDesktop} />
             <Route path="/" exact component={NookPhoneHome} />
             <Route path="/maps" exact component={NookPhoneMaps} />
+            <Route path="/archive/video01" exact component={NookPhoneStreetVideo} />
+
             <Route path="/reminders" exact component={NookPhoneReminders}/>
             <Route path="/suspects" exact component={NookPhoneSuspects}/>
             <Route path="/instanook" exact component={NookPhoneInstaNook}/>
